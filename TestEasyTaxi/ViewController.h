c0f9ab47a3ded8bf391e76b5d448549b33a5948d
anafinotti <anafinotti@hotmail.com>
2016-01-24 14:19:50 -0200

    initialization

---
+++ TestEasyTaxi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestEasyTaxi
+//
+//  Created by Ana Finotti on 1/24/16.
+//  Copyright © 2016 Finotti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestEasyTaxi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestEasyTaxi
+//
+//  Created by Ana Finotti on 1/24/16.
+//  Copyright © 2016 Finotti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
